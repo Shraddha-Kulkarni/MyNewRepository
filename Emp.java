f0c80e91142584fe72039c15951031bca3541a2f
Shraddha-Kulkarni <60878981+Shraddha-Kulkarni@users.noreply.github.com>
2020-02-11 15:16:31 +0530

    Add files via upload

---
+++ Emp.java
@@ -0,0 +1,25 @@
+package jdbcPack;
+
+public class Emp {
+	int id;
+	String name;
+	float salary;
+	Emp(){}
+	Emp(int id,String name, float salary)
+	{
+		this.id=id;
+		this.name=name;
+		this.salary=salary;
+	}
+	void calculatesalary()
+	{
+		System.out.println("salary is calculated"+(salary+200));
+	}
+
+	public static void main(String[] args) {
+		Emp e=new Emp();
+		e.calculatesalary();
+
+	}
+
+}
